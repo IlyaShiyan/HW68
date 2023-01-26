1fed9d449b6dd41f7bc2b0e3f9653f8f78534657
Ilya Shiyan <ilya.shiyan97@mail.ru>
2023-01-26 13:49:55 +0300

    Finished project

---
+++ src/main/java/Main.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+
+        BufferedReader bufferedReader =new BufferedReader(new InputStreamReader(System.in));
+        int sum = 0;
+        try {
+            while (true) {
+                String str = bufferedReader.readLine();
+                if (".".equals(str)){
+                    System.out.println(sum);
+                break;
+                }else
+                    sum+=Integer.parseInt(str);
+
+            }} catch (IOException e) {
+            e.printStackTrace();
+        } catch (NumberFormatException exception) {
+            System.out.println("Enter number");
+        }
+
+
+    }
+}
