8b25dd4f1de66e24785629c97b7aba909e4125c8
Ilya Shiyan <ilya.shiyan97@mail.ru>
2023-01-26 14:26:37 +0300

    refactoring

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,25 +3,23 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
 
-        BufferedReader bufferedReader =new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         int sum = 0;
         try {
             while (true) {
                 String str = bufferedReader.readLine();
-                if (".".equals(str)){
+                if (".".equals(str)) {
                     System.out.println(sum);
-                break;
-                }else
-                    sum+=Integer.parseInt(str);
-
-            }} catch (IOException e) {
+                    break;
+                } else
+                    sum += Integer.parseInt(str);
+            }
+        } catch (IOException e) {
             e.printStackTrace();
         } catch (NumberFormatException exception) {
             System.out.println("Enter number");
         }
-
-
     }
 }
